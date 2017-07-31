f9b6cd228d77ffde5435bc5886724769104d1881
Andrew Madonna <andrew.w.madonna@gmail.com>
2017-07-31 11:54:10 -0400

    Add more auth test coverage

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,16 +1,32 @@
-from app_fixtures import app
+import re
+from datetime import datetime
+
+from app_fixtures import app, db
 from restful_ben.test_utils import json_call, login, dict_contains, iso_regex
 
 def test_login(app):
     test_client = app.test_client()
 
     response = json_call(test_client.post, '/session', username='amadonna', password='foo')
-    print(response.headers)
-    ## TODO: !!! inspect cookie
-    ## TODO: inspect test_client cookie jar?
+
     assert response.status_code == 201
     assert 'csrf_token' in response.json
     assert len(response.json['csrf_token']) > 64
+    assert 'Set-Cookie' in response.headers
+
+    cookie_regex = r'session=[^;]+;\sExpires=[A-Za-z]{3},\s[0-9]{2}\s[A-Za-z]{3}\s[0-9]{4}\s[0-9]{2}:[0-9]{2}:[0-9]{2}\sGMT;\sHttpOnly'
+
+    matches = re.match(cookie_regex, response.headers['Set-Cookie'])
+    assert matches != None
+
+def test_login_fail(app):
+    test_client = app.test_client()
+
+    response = json_call(test_client.post, '/session', username='amadonna', password='bar')
+    assert response.status_code == 401
+
+    response = json_call(test_client.post, '/session', username='notauser', password='foo')
+    assert response.status_code == 401
 
 def test_get_session(app):
     test_client = app.test_client()
@@ -18,6 +34,10 @@ def test_get_session(app):
 
     response = test_client.get('/session')
     assert response.status_code == 204
+
+    test_client = app.test_client()
+    response = test_client.get('/session')
+    assert response.status_code == 401
 
 def test_logout(app):
     test_client = app.test_client()
@@ -27,12 +47,22 @@ def test_logout(app):
     assert response.status_code == 204
 
     response = test_client.delete('/session')
-    print(response.headers)
-    ## TODO: inspect cookie in response
-    ## TODO: assert token revoked_at
     assert response.status_code == 204
+    assert 'Set-Cookie' in response.headers
+    assert response.headers['Set-Cookie'] == 'session=deleted; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly'
+
+    with app.app_context():
+        token = db.session.query(app.auth.token_model).one()
+
+    assert isinstance(token.revoked_at, datetime)
 
     response = test_client.get('/session')
+    assert response.status_code == 401
+
+def test_requires_login(app):
+    test_client = app.test_client()
+
+    response = json_call(test_client.get, '/cats')
     assert response.status_code == 401
 
 def test_csrf(app):
